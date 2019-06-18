9ac7893ace1b790ac228b0b60610cff9deca268b
zeon1992 <beqa.qarqusashvili.20@gmail.com>
2019-06-18 13:44:03 +0400

    fix(source): Remove index.js file

--- main.mjs
+++ main.mjs
@@ -7,52 +7,10 @@ const scenario = [
             title: 'Title 1',
             description: 'This action is responsible for reading the most popular customers'
         },
-                // callback for main execution
-        // silent: true,
         call: async (store) => {
-            // await sleep(1000);
             store.surname = 'Jon'
-            // throw new Error('first error');
         },
-				// callback for rollback
-        // restore: async () => {
-           
-        // }
-    },
-    {
-        index: 2,
-        meta: {
-            title: 'Title 2',
-            description: 'This action is responsible for reading the most popular customers'
-        },
-        silent: true,
-				// callback for main execution
-        call: async (store) => {
-            store.name = 'Jon'
-            await sleep(2000);
-            throw new TypeError('name is not defiend');
-
-        },
-				// callback for rollback
         restore: async () => {}
-    },
-    {
-        index: 3,
-        meta: {
-            title: 'Title 3',
-            description: 'This action is responsible for reading the most popular customers'
-        },
-        // silent: true,
-				// callback for main execution
-        call: async (store) => {
-            store.name = 'Jon'
-            // throw new TypeError('name is not defiend');
-            // await sleep(2000);
-        },
-				// callback for rollback
-        // restore: async (store) => {
-        //     throw new TypeError('name is not defiend');
-        // }
     }
 ];
 
@@ -60,16 +18,13 @@ const transaction = new Transaction();
 
 (async() => {
     try {
-			await transaction.dispatch(scenario);
-			const store = transaction.store; // {} | null
-            const logs = transaction.logs; // []
-            console.log(logs);
-            console.log(store);
-            // console.log(logs[1].error);
-            
+        await transaction.dispatch(scenario);
+        const store = transaction.store; // {} | null
+        const logs = transaction.logs; // []
+        console.log(logs);
+        console.log(store);
     } catch (err) {
-            // Send email about broken transaction
-            console.log(err);
+        console.log(err);
     }
 })();
 
